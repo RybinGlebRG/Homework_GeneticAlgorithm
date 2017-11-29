a35df48c6d22bcac3a662351e8aa6056b9be07cd
Gleb <rybin.gleb.r.g@gmail.com>
2017-11-29 20:55:42 +0300

    Made my own

--- evo.py
+++ evo.py
@@ -1,30 +1,180 @@
 import reader
+import random
 
 class Evo():
     knapsack=None
     items=None
+    popN=1
+    n=0
+    fittestPopulation=None
 
-    def __init__(self):
+    def __init__(self,n):
+        self.n=n
         rd = reader.Reader()
         f = rd.read("28.txt")
         self.knapsack = rd.getKnapsack(f)
         self.items = rd.getItems(f)
 
+    def getWVV(self,individual):
+        weight=0
+        volume=0
+        value=0
+        for i in range(0,len(individual)):
+            if individual[i]==1:
+                weight+=self.items[i][0]
+                volume += self.items[i][1]
+                value += self.items[i][2]
+        return weight,volume,value
+
+    def isFit(self,weight,volume):
+        if weight> self.knapsack[0] or volume>self.knapsack[1]:
+            return False
+        else:
+            return True
+
     def getStartPopulation(self,n):
+        population=[]
+        individual=[0 for i in self.items]
+        while len(population)<n:
+            i=random.randint(0,len(self.items))
+            for j in range(i,len(individual)):
+                individual[j]=1
+                weight,volume,value=self.getWVV(individual)
+                if not self.isFit(weight,volume):
+                    individual[j]=0
+                    break
+            for j in range(0,i):
+                individual[j]=1
+                weight,volume,value=self.getWVV(individual)
+                if not self.isFit(weight,volume):
+                    individual[j]=0
+                    break
+            population.append(individual)
+        return population
 
-        pass
+    def selection(self,population):
+        amount=self.n*0.2
+        selected=[]
+        while len(selected)<amount:
+            max=0
+            for i in range(0,len(population)):
+                if population[i] not in selected:
+                    max=i
+            for i in range(max+1,len(population)):
+                weight, volume, value = self.getWVV(population[i])
+                mw,mv,mva=self.getWVV(population[max])
+                if population[i] not in selected and value>mva:
+                    max=i
+            selected.append(population[max])
+        return selected
 
-    def selection(self):
-        pass
+    def crossbreeding(self,selected):
+        newborn=[]
 
-    def crossbreeding(self,ind1,ind2):
-        pass
+        while len(newborn)<self.n*0.2:
+            points=[]
+            children=[]
+            parents=[]
+            points.append(0)
+            while len(points)<3:
+                i=random.randint(0,len(selected[0])-1)
+                if i not in  points:
+                    points.append(i)
+            points.sort()
+            parents.append(selected.pop(0))
+            parents.append(selected.pop(0))
+            for j in range(0,2):
+                parent = 0
+                indexes = [0 for i in parents[0]]
+                for i in range(0,len(indexes)):
+                    if i in points:
+                        parent=random.randint(0,1)
+                    indexes[i]=parent
+                child=[]
+                for i in range(0,len(indexes)):
+                    child.append(parents[indexes[i]][i])
+                children.append(child)
+            w,v,va=self.getWVV(children[0])
+            w2,v2,va2=self.getWVV(children[1])
+            if self.isFit(w,v) and self.isFit(w2,v2):
+                newborn.append(children[0])
+                newborn.append(children[1])
+        return newborn
 
-    def mutation(self):
-        pass
+    def mutation(self,newborn):
 
-    def newPopulation(self):
-        pass
+        i=random.randint(0,len(newborn)-1)
+        for j in range(0,len(newborn[i])):
+            newborn[i][j]^=1
+        return newborn
 
-    def evaluation(self):
-        pass
+
+    def newPopulation(self,mutated,population):
+        # Хранит индексы
+        worst=[]
+        while len(worst)<self.n*0.2:
+            min=0
+            for i in range(0, len(population)):
+                if i not in worst:
+                    min = i
+            for i in range(min+1,len(population)):
+                weight, volume, value = self.getWVV(population[i])
+                mw,mv,mva=self.getWVV(population[min])
+                if i not in worst and value<mva:
+                    min=i
+            worst.append(min)
+        newPopulation=[]
+
+        for el in population:
+            newPopulation.append(el)
+        for i in range(0,len(worst)):
+            newPopulation[worst[i]]=mutated[i]
+        return newPopulation
+
+
+
+    def evaluation(self,newPopulation,population):
+        maxP = 0
+        for i in range(1, len(population)):
+            weight, volume, value = self.getWVV(population[i])
+            mw, mv, mva = self.getWVV(population[maxP])
+            if  value > mva:
+                maxP = i
+        maxN = 0
+        for i in range(1, len(newPopulation)):
+            weight, volume, value = self.getWVV(newPopulation[i])
+            mw, mv, mva = self.getWVV(newPopulation[maxN])
+            if  value > mva:
+                maxN = i
+
+        if min(maxP,maxN)*0.1+min(maxP,maxN)>max(maxP,maxN):
+            return True
+        else:
+            return False
+
+    def routine(self,population):
+        selected=self.selection(population)
+        newborn=self.crossbreeding(selected)
+        mutated=self.mutation(newborn)
+        newPopulation=self.newPopulation(mutated,population)
+        isConverged=self.evaluation(newPopulation,population)
+        population=newPopulation
+        self.popN+=1
+        return isConverged
+
+    def check(self,fittestPopulation):
+        for individual in fittestPopulation:
+            w,v,va=self.getWVV(individual)
+            if w>self.knapsack[0] or v>self.knapsack[1]:
+                raise Exception("Incorrect weight or volume")
+
+
+    def run(self):
+        population=self.getStartPopulation(self.n)
+
+        while True:
+            isConverged=self.routine(population)
+            if isConverged or self.popN>=100:
+                break
+        self.fittestPopulation=population
+        self.check(self.fittestPopulation)
---
+++ lib.py
@@ -0,0 +1,82 @@
+
+def run():
+
+    import reader
+
+    import random
+
+    rd=reader.Reader()
+    f=rd.read("28.txt")
+    ks=rd.getKnapsack(f)
+    items=rd.getItems(f)
+
+
+    from deap import creator
+    from deap import base
+    from deap import tools
+    from deap import algorithms
+
+    creator.create("Fitness", base.Fitness, weights=(-1.0, -1.0, 1.0))
+    creator.create("Individual", set, fitness=creator.Fitness)
+
+    toolbox = base.Toolbox()
+
+    toolbox.register("attr_item", random.randrange, rd.n)
+    toolbox.register("individual", tools.initRepeat, creator.Individual,
+                     toolbox.attr_item, 50)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    def evalKnapsack(individual):
+        weight = 0.0
+        volume = 0.0
+        value = 0.0
+        for index in individual:
+            weight += items[index][0]
+            volume += items[index][1]
+            value += items[index][2]
+        if len(individual) > rd.n or weight > ks[0] or volume>ks[1]:
+            return 30000, 15000, 0  # Ensure overweighted bags are dominated
+        return weight, volume, value
+
+    def cxSet(ind1, ind2):
+        temp = set(ind1)
+        ind1 &= ind2
+        ind2 ^= temp
+        return ind1, ind2
+
+    def mutSet(individual):
+        """Mutation that pops or add an element."""
+        if random.random() < 0.5:
+            if len(individual) > 0:  # We cannot pop from an empty set
+                individual.remove(random.choice(sorted(tuple(individual))))
+        else:
+            individual.add(random.randrange(rd.n))
+        return individual,
+
+    toolbox.register("evaluate", evalKnapsack)
+    toolbox.register("mate", cxSet)
+    toolbox.register("select", tools.selNSGA2)
+    toolbox.register("mutate", mutSet)
+
+    def main():
+        random.seed(64)
+        NGEN = 50
+        MU = 50
+        LAMBDA = 100
+        CXPB = 0.7
+        MUTPB = 0.2
+
+        pop = toolbox.population(n=MU)
+
+        algorithms.eaMuPlusLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN)
+
+        return pop
+
+
+    pop=main()
+    big=0
+    for i in range(0,len(pop)):
+        if pop[i].fitness.values[2]>pop[big].fitness.values[2]:
+            big=i
+    new=list(pop[big])
+    print(new)
